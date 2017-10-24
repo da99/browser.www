ade9e9aa34d092c3d43d3131fc9290bb84c02451
da99 <i-hate-spam-1234567@mailinator.com>
2017-10-23 20:05:57 -0500

    Browser results set to the console.

--- specs/browser/server.cr
+++ specs/browser/server.cr
@@ -1,9 +1,20 @@
 
 require "da_router"
 require "json"
+require "colorize"
 
 PORT = 3010
 
+
+def pass_fail_color(s : String)
+  s = s.gsub(/PASS([a-z]{2})?/i) do |s|
+    s.colorize(:green)
+  end
+  s = s.gsub(/FAIL([a-z]{2})?/i) do |s|
+    s.colorize(:red)
+  end
+  s
+end # === def pass_fail_color
 
 class Router
 
@@ -11,6 +22,8 @@ class Router
   @@tmp_file = HTTP::StaticFileHandler.new("tmp", false)
 
   include DA_ROUTER
+
+  @request_body = ""
 
   def resp
     ctx.response
@@ -36,14 +49,14 @@ class Router
     ctx.response << str
   end # === def write_html
 
-  def self.json(ctx, o)
+  def self.write_json(ctx, o)
     r = ctx.response
     r.content_type = "application/json"
     r.headers["Access-Control-Allow-Origin"] = "*"
     r << o.to_json
   end # === def self.json
 
-  def json(o)
+  def write_json(o)
     r = ctx.response
     r.content_type = "application/json"
     r.headers["Access-Control-Allow-Origin"] = "*"
@@ -58,16 +71,16 @@ class Router
   get("/favicon.ico") { @@static_file.call(ctx) }
   get("/css/:name") { @@static_file.call(ctx) }
 
-  get("/browser.specs.js") {
+  get("/browser.js") {
     @@tmp_file.call(ctx)
   }
 
   post("/") do
-    json({when: "for now"})
+    write_json({when: "for now"})
   end
 
   get("/_csrf") do
-    json({_csrf: "some_value"})
+    write_json({_csrf: "some_value"})
   end
 
   post("/html") do
@@ -87,16 +100,40 @@ class Router
     write_text("Some plain text.");
   end
 
+  post("/to-console") do
+    puts "Browser: #{pass_fail_color request_body}"
+    write_json({success: "Received: #{request_body}"})
+  end
+
   def request_body
-    (ctx.request.body || "").to_s
+    return @request_body unless @request_body.empty?
+    origin = ctx.request.body
+    return "" unless origin
+
+    len = (ctx.request.content_length || 0).to_i
+    raise Exception.new("Body too big.") if len > 1_000_000
+
+    return "" if len < 1
+    return "" unless origin
+    @request_body = ( origin.gets(len) || raise Exception.new("No body sent.") )
   end
 
   post("/repeat") do
-    json({ok: true, data: request_body, about: "data received: #{request_body.to_json}" })
+    write_json({ok: true, data: request_body, about: "data received: #{request_body.to_json}" })
   end
 
   post("/json") do
-    json({msg: "get smart"})
+    write_json({msg: "get smart"})
+  end
+
+  get("/do-reload") do
+    file = "tmp/do-reload"
+    msg = if File.exists?(file)
+            File.stat(file).mtime.to_s
+          else
+            ""
+          end
+    write_json({msg: msg})
   end
 
   # post("/client-error-to-stdout") do
@@ -112,7 +149,7 @@ class Router
   def self.fulfill(ctx)
     DA_ROUTER.route!(ctx)
     ctx.response.status_code = 404
-    json(ctx, {msg: "not found: #{ctx.request.method} #{ctx.request.path}"})
+    write_json(ctx, {msg: "not found: #{ctx.request.method} #{ctx.request.path}"})
     route(ctx) do
     end
   end # === def self.fulfill
